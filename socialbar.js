7a87b2dbb7c9e576cce6b4092e22089ca1e9bde1
Ilma Ilfiyah <65756250+ilmailfiyah@users.noreply.github.com>
2026-05-13 05:11:09 +0700

    Update socialbar.js

--- socialbar.js
+++ socialbar.js
@@ -1,1 +1,3 @@
-document.write("<script src='//walkeralacrityfavorite.com/91/43/e5/9143e5c2464e8f4bc3b0c38344491e04.js' type='text/javascript'><\/script>");
+var s = document.createElement("script");
+s.src = "https://walkeralacrityfavorite.com/91/43/e5/9143e5c2464e8f4bc3b0c38344491e04.js";
+document.body.appendChild(s);
