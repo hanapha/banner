7c45c70083f8d1c6b80a1f158e4b76bbc2fc6563
Irawan Ria <irawanria85@gmail.com>
2026-05-19 11:31:19 +0700

    Create ad1.js

---
+++ ad1.js
@@ -0,0 +1,12 @@
+document.write(`
+<script>
+  atOptions = {
+    'key' : 'eef20bf05299b36d6c9d33b6072ab986',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+<\/script>
+<script src="//walkeralacrityfavorite.com/eef20bf05299b36d6c9d33b6072ab986/invoke.js"><\/script>
+`);
